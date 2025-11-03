fce8113249d02735e9c7baef759b8e36a4aa403d
yijuchoo <yirou.choo@gmail.com>
2025-11-03 23:06:01 +0800

    Added comments for MinMaxArray2

--- src/foundations/activites/MinMaxArray2.java
+++ src/foundations/activites/MinMaxArray2.java
@@ -12,9 +12,14 @@ public class MinMaxArray2 {
             // Sentinel value: no valid maximum
             return Integer.MIN_VALUE;
         }
-
+        // Assuming the first element of the array (arr[0]) is the smallest value
         int maxValue = arr[0];
+        // loop starts from index 1, not 0, because:
+        //You already used arr[0] as your initial minValue.
+        //There’s no need to compare it with itself — arr[0] < arr[0] will never be true
         for (int i = 1; i < arr.length; i++) {
+            // For each element, you check if it’s smaller than the current minValue.
+            //If yes, you update minValue to that smaller number
             if (arr[i] > maxValue) {
                 maxValue = arr[i];
             }
